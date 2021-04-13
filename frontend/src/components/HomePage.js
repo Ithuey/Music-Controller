ba327660b0120c2d7b9f6034808137663b41b35a
Ithuey <thueycarp@gmail.com>
2021-04-13 11:54:29 -0400

    added leave room/ delete room code

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -16,7 +16,8 @@ export default class HomePage extends Component {
     super(props);
     this.state = {
       roomCode: null,
-    }
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -52,6 +53,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode(){
+    this.setState({
+      roomCode: null,
+    })
+  }
+
   render() {
     return (
       <Router>
@@ -70,7 +77,10 @@ export default class HomePage extends Component {
                    
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route path="/room/:roomCode"
+          render={(props) => {
+            return <Room {...props} leaveRoomCallback={this.clearRoomCode}/>
+          }}/>
         </Switch>
       </Router>
     );
--- music_controller/settings.py
+++ music_controller/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u=--wd#p&u#et=cu-=iz2j^po2v=30qv@+z&g6#2pcupb-82s9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
